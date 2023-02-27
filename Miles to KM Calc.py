2db20a6411fa9f422bbe5ec4810a73f2750f2772
Shistin <126476104+Shistin@users.noreply.github.com>
2023-02-27 14:43:28 +0200

    Add files via upload

---
+++ Miles to KM Calc.py
@@ -0,0 +1,11 @@
+# User Input
+print("Miles to Kilometers Calculator")
+
+while True:
+    
+    miles = int(input('How Many Miles: '))
+    kilometers = miles * 1.609344
+    print(f"{miles} miles is {kilometers} kilometers")
+    
+    pass
+
---
+++ mL to fL OZ.py
@@ -0,0 +1,11 @@
+# User Input
+print("mL to fL OZ Calculator")
+
+while True:
+    
+    ml = int(input('How Many mL: '))
+    floz = ml / 29.574
+    print(f"{ml} mL is {floz} fL OZ")
+    
+    pass
+
